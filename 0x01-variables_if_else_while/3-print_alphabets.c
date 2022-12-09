15eaffa97fbe595e5cd650a31e9f3434cca3b827
oghene-ella <oghenekaro57@gmail.com>
2022-12-09 12:35:36 -0800

    abeg just work. task 5, 6, 7

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,20 +8,22 @@
 int main(void)
 {
 	char c = 'a';
-	 while (c <= 'z')
-	 {
- 		 putchar(c);
-		 c++;
-	 }
+
+	while (c <= 'z')
+      	{
+
+ 		putchar(c);
+		c++;
+       	}
 	 
-	 c = 'A';
+	c = 'A';
 
-	 while (c <= 'Z')
-	 {
-		 putchar(c);
-		 c++;
-	 }
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
 
-	 putchar('\n');
-	 return (0);
+	putchar('\n');
+	return (0);
 }
