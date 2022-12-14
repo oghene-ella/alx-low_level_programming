1c7afadc45b88cdcc5b975716397e3448a6478e7
oghene-ella <oghenekaro57@gmail.com>
2022-12-13 21:04:45 -0800

    task 15 and 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@
  * Description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
- */
+*/
 int main(void)
 {
 	unsigned long int i;
