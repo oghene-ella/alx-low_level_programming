703846d133e0367daf0bc37e4af63349221e43b2
oghene-ella <oghenekaro57@gmail.com>
2022-12-13 20:59:31 -0800

    task 12 and 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - prints times table
  * @n : times table to use
  * Return:void
- */
+*/
 
 void print_times_table(int n)
 {
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,7 +5,7 @@
  * Description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
- */
+*/
 int main(void)
 {
 	int c = 0;
