11634754ab415bd954e03725662c2e111782cfbd
Oghenekaro Emmanuella <oghenekaro57@gmail.com>
2022-12-14 05:08:32 +0100

    Update 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
