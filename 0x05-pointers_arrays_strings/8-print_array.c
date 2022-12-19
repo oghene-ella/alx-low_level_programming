8db5c1660e7847ff41790d235e8caedd9a52909c
oghene-ella <oghenekaro57@gmail.com>
2022-12-19 05:06:21 -0800

    taask 6, 7, 8, 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,7 @@
  * Description: Numbers must be separated by comma and space.
  * Numbers should be displayed in the same order they are stored in array.
  * You can only use _putchar to print.
- */
+*/
 
 void print_array(int *a, int n)
 {
