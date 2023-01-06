1bb85d73ac38a0583e4fdeec3f35279084005edd
oghene-ella <oghenekaro57@gmail.com>
2023-01-06 10:09:33 -0800

    updated a file

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -5,14 +5,12 @@
  * @c:  is the int that will use for the argument of the function
  * Return: 1 if uppercase character 0 if not
 */
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-
 	else
 		return (0);
 }
